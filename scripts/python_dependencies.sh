11ae159a42843421013c43132ae111fd5f43bd36
rushabhselokar1 <rushabhselokar17@gmail.com>
2024-01-15 17:42:33 +0530

    update 9

--- scripts/python_dependencies.sh
+++ scripts/python_dependencies.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo chown -R ubuntu:ubuntu ~/pythonprojeckdjango
-virtualenv /home/ubuntu/pythonprojeckdjango/venv
-source /home/ubuntu/pythonprojeckdjango/venv/bin/activate
-pip install -r /home/ubuntu/pythonprojeckdjango/requirements.txt
+#sudo chown -R ubuntu:ubuntu ~/pythonprojeckdjango
+#virtualenv /home/ubuntu/pythonprojeckdjango/venv
+#source /home/ubuntu/pythonprojeckdjango/venv/bin/activate
+#pip install -r /home/ubuntu/pythonprojeckdjango/requirements.txt
