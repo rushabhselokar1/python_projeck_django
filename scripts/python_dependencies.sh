f2fd3f500548e496ccd4cfdf9d31884f881ac8d1
rushabhselokar1 <rushabhselokar17@gmail.com>
2024-01-15 18:11:34 +0530

    update 12

--- scripts/python_dependencies.sh
+++ scripts/python_dependencies.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo chown -R ubuntu:ubuntu ~/pythonprojeckdjango
+#sudo chown -R ubuntu:ubuntu ~/pythonprojeckdjango
 python -m venv myenv
 #virtualenv /home/ubuntu/pythonprojeckdjango/venv
 source /home/ubuntu/pythonprojeckdjango/myenv/bin/activate
