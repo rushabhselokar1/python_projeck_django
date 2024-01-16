4dc650f586b75a32f3d830cc161ea2d9d7ba0d2a
rushabhselokar1 <rushabhselokar17@gmail.com>
2024-01-16 11:09:42 +0530

    update 19

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -4,8 +4,8 @@
 sudo systemctl daemon-reload
 sudo rm -f /etc/nginx/sites-enabled/default
 
-sudo cp /home/ubuntu/pythonprojeckdjango/nginx/nginx.conf /etc/nginx/sites-available/aip_web_project
-sudo ln -s /etc/nginx/sites-available/aip_web_project /etc/nginx/sites-enabled/
+sudo cp /home/ubuntu/pythonprojeckdjango/nginx/nginx.conf /etc/nginx/sites-available/pythonprojeckdjango
+sudo ln -s /etc/nginx/sites-available/pythonprojeckdjango /etc/nginx/sites-enabled/
 #sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled
 #sudo nginx -t
 sudo gpasswd -a www-data ubuntu
